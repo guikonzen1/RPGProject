fcccc67f3bb4d0bf9f0c1c1e526d830256a73d11
Guilherme Konzen <guilherme.konzen@sou.ufmt.br>
2025-03-27 00:33:47 -0400

    Player Movement Basic Setup
    Teste da movimentação

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public Rigidbody2D rb;
+   private Rigidbody2D _rb;
+
+   public float moveSpeed;
+   private Vector2 _moveDirection;
    
-    
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
-    }
+   public InputActionReference moveAction;
+   public InputActionReference runAction;
+
+   void Start()
+   {
+      _rb = GetComponent<Rigidbody2D>();
+   }
+   private void Update()
+   {
+      _moveDirection = moveAction.action.ReadValue<Vector2>();
+   }
+
+   private void FixedUpdate()
+   {
+      _rb.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+   }
+
+   private void OnEnable()
+   {
+      runAction.action.started += Run;
+   }
+
+   private void OnDisable()
+   {
+      runAction.action.started -= Run;
+   }
+   private void Run(InputAction.CallbackContext obj)
+   {
+      Debug.Log("Correndo");
+   }
 }
